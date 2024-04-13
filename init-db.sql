c3070b979d9440c579954de9b3ee78a78fda1d6a
nguyen van cuong <156575951+iuh20102161@users.noreply.github.com>
2024-04-13 14:05:01 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS tblbookIUHX (
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH20102161'),
+  ('Big Data', 'IUH20102161'),
+  ('Cloud Deployement', 'IUH20102161'),
+('Data Analysis', 'IUH20102161'),
+('Block Chain', 'IUH20102161');
